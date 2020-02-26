454229735d352253b6cfe5598d8491752e15c014
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-02-26 16:34:04 +0900

    🥦 feat: add addBook

--- index.js
+++ index.js
@@ -4,29 +4,22 @@ const books = require('./data');
 
 // via [Basic Apollo app - CodeSandbox](https://codesandbox.io/s/nn9y2wzyw4)
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     book(author: String!): Book
     books: [Book]
   }
+  
+  type Mutation {
+    addBook(title: String!, author: String!): Book!
+  }
 `;
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
     // parent: 上位のリゾルバで解決した結果を含むオブジェクト
@@ -38,6 +31,16 @@ const resolvers = {
     },
     books: () => books,
   },
+  Mutation: {
+    addBook(parent, args, context, info) {
+      const post = {
+        title: args.title,
+        author: args.author,
+      };
+      books.push(post);
+      return post;
+    },
+  }
 };
 
 // The ApolloServer constructor requires two parameters: your schema
